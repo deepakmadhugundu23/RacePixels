4bdbc21c65c604e228caae0a0a7dfb0d444e6ef1
M Sai Deepak Chandra <madhugundudeepak67@gmail.com>
2026-06-01 14:07:09 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+## RacePixels
+RacePixels is a fun activity where two pixels race each other to claim the boundaries. This can sound like a simulation game without downloading any shit app. 
+This racepixels can turn a boring image into an colorful image by racing and competing themselves by acquiring more territory within the boundary.
+---
+**How to run?**
+1. Clone my repository or download the zip.
+2. Download the requirements.txt
+3. Create your own input.bmp with your own design in your mind. Add some color seeds.
+4. Run the main.py and it will generate the output.bmp
+5. Just enjoy the output.
+---
+**Credits to Honza**
+**Note**: This is for audience only in Code Buddies and GitCoding Arena server.
---
+++ main.py
@@ -0,0 +1,29 @@
+import random
+import numpy as np
+from PIL import Image
+
+image = Image.open("input.bmp").convert("RGB")
+data = np.array(image)
+
+def growpixels(grid):
+    newgrid = grid.copy()
+    rows, cols, channels = grid.shape
+
+    coords = [(r, c) for r in range(1, rows-1) for c in range(1, cols-1)]
+    random.shuffle(coords)
+
+    for r, c in coords:
+        if np.all(grid[r, c] == [255, 255, 255]):
+            coordinates = [grid[r-1, c], grid[r+1, c], grid[r, c-1], grid[r, c+1]]
+            for n in coordinates:
+                if not np.all(n == [255, 255, 255]) and not np.all(n == [0, 0, 0]):
+                    newgrid[r, c] = n
+                    break
+    return newgrid 
+
+for _ in range(200):
+    data = growpixels(data)
+
+resultimage = Image.fromarray(data)
+resultimage.save("output.bmp")
+print("check output.bmp")
