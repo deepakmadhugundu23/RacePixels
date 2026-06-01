92d2e9aa08aabcb7ad21d8fddb7dbf1980d9a6cf
M Sai Deepak Chandra <madhugundudeepak67@gmail.com>
2026-06-01 17:11:59 +0530

    Replace racepixels with growpixels function

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
-import random
+import sys, random, time, ctypes
 import numpy as np
 from PIL import Image
+from colorama import init, Fore, Style
+
+init(autoreset=True)
 
 image = Image.open("input.bmp").convert("RGB")
 data = np.array(image)
 
-def racepixels(grid):
+def growpixels(grid):
     newgrid = grid.copy()
-    rows, cols, channels = grid.shape
-
+    rows, cols, _ = grid.shape
     coords = [(r, c) for r in range(1, rows-1) for c in range(1, cols-1)]
     random.shuffle(coords)
-
     for r, c in coords:
-        if np.all(grid[r, c] == [255, 255, 255]):
-            coordinates = [grid[r-1, c], grid[r+1, c], grid[r, c-1], grid[r, c+1]]
-            for n in coordinates:
-                if not np.all(n == [255, 255, 255]) and not np.all(n == [0, 0, 0]):
+        if (grid[r, c] == [255, 255, 255]).all():
+            neighbors = [grid[r-1, c], grid[r+1, c], grid[r, c-1], grid[r, c+1]]
+            for n in neighbors:
+                if not (n == [255, 255, 255]).all() and not (n == [0, 0, 0]).all():
                     newgrid[r, c] = n
                     break
-    return newgrid 
+    return newgrid
+
+PIXEL = "█"
+
+def printgrid(grid):
+    sys.stdout.write("\033[H")
+    for row in grid:
+        line = ""
+        for p in row:
+            if (p == [255,0,0]).all(): line += "\033[31m" + PIXEL + "\033[0m"
+            elif (p == [0,0,255]).all(): line += "\033[34m" + PIXEL + "\033[0m"
+            elif (p == [0,0,0]).all(): line += "█"
+            else: line += " "
+        sys.stdout.write(line + "\n")
+    sys.stdout.flush()
+
+sys.stdout.write("\033[2J")
 
 for _ in range(200):
-    data = racepixels(data)
+    data = growpixels(data)
+    printgrid(data)
+    time.sleep(0.05)
 
-resultimage = Image.fromarray(data)
-resultimage.save("output.bmp")
+Image.fromarray(data).save("output.bmp")
 print("check output.bmp")
